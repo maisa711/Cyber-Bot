0c90c022e037a3be68f037c3bff9a583fb470622
zorozoro999 <43515290+zorozoro999@users.noreply.github.com>
2019-03-26 23:05:32 +0400

    Add files via upload

---
+++ install_lib.cmd
@@ -0,0 +1,10 @@
+:A
+python -m pip install gtts
+python -m pip install speech_recognition
+python -m pip install mutagen.mp3
+python -m pip install re
+python -m pip install urllib
+python -m pip install opencv-python
+python -m pip install wikipedia
+
+goto :A
